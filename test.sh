3fda5bd3a1a3c50d42d03ca41857f421a6301b89
lawtlee <lawsac2947@gmail.com>
2023-07-28 18:40:51 -0700

    Added Blogpost Workflow
    Workflow checks if there are changes to _posts
    (deletion or addition). If there are, it will make changes
    to the logic accordingly.

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+modals=$(expr $(grep -E 'plus:.*' _includes/modals.html | grep -oE '\d+') + 0)
+portfolio=$(expr $(grep -E 'plus:.*' _includes/portfolio_grid.html | grep -oE '\d+') + 0)
+
+post_count=$(expr $(ls -p _posts | wc -l) + 0)
+
+if [ post_count != modals ]; then
+    old_pattern="{% assign modified_index = forloop.index | plus: $modals %}"
+    new_pattern="{% assign modified_index = forloop.index | plus: $post_count %}"
+    sed "s/$old_pattern/$new_pattern/" _includes/modals.html > temp.txt
+    cat < temp.txt > _includes/modals.html
+    rm -f temp.txt
+fi
+
+if [ post_count != portfolio ]; then
+    old_pattern="{% assign modified_index = forloop.index | plus: $portfolio %}"
+    new_pattern="{% assign modified_index = forloop.index | plus: $post_count %}"
+    sed "s/$old_pattern/$new_pattern/" _includes/portfolio_grid.html > temp.txt
+    cat < temp.txt > _includes/portfolio_grid.html
+    rm -f temp.txt
+fi
